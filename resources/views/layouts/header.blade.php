ee565853d4cbd32975bd2f1a844168456295d8d4
talha5131 <talhamalik4147@gmail.com>
2020-12-03 15:39:03 +0500

    Header Update

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -37,9 +37,6 @@
     <script src="public/assets/js/splide.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/animejs/2.0.2/anime.min.js"></script>
     <script>
-        // Scroll To Top Button//
-
-        
         window.onscroll = function() {
             scrollFunction()
         };
@@ -47,11 +44,11 @@
         function scrollFunction() {
 
             if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
-                document.getElementById("header-1").style.background = "transparent";
-                // console.log("hello world")
+                document.getElementById("header-1").style.display = "none";
+                document.getElementById("header-2").style.display = "block";
             } else {
-                document.getElementById("header-1").style.background = "black";
-                // console.log("hello")
+                document.getElementById("header-1").style.display = "block";
+                document.getElementById("header-2").style.display = "none";
             }
         }
     </script>
@@ -61,12 +58,12 @@
 
 <body>
 
-    <div id = "header-1" class="fixed-top">
+    <div id="header-1" class="fixed-top">
         <div class="wrap" style="z-index: 100; position:absolute;">
             <div class="container">
                 <div class="row">
                     <div class="col-md-12">
-                        <div class="bg-wrap" >
+                        <div>
                             <div class="row">
                                 <div class="col-md-6 d-flex align-items-center">
                                     <p class="mb-0 phone pl-md-2">
@@ -89,7 +86,63 @@
                 </div>
             </div>
         </div>
-        <nav class="navbar navbar-expand-lg navbar-dark ftco_navbar bg-dark ftco-navbar-light" id="ftco-navbar" style="position: absolute; top: 45px; left:80px; z-index: 100;">
+        <nav class="navbar navbar-expand-lg navbar-dark ftco_navbar bg-dark ftco-navbar-light" id="ftco-navbar" style="position: absolute; top: 40px; z-index: 100; width: 100%;">
+            <div class="container">
+                <a class="navbar-brand" href="{{url('/')}}"><img class="img-fluid" src="public/assets/ved-logo-2.png"></a>
+                <!-- <form action="#" class="searchform order-sm-start order-lg-last">
+                <div class="form-group d-flex">
+                    <input type="text" class="form-control pl-3" placeholder="Search">
+                    <button type="submit" placeholder="" class="form-control search"><span class="fa fa-search"></span></button>
+                </div>
+            </form> -->
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#ftco-nav" aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="fa fa-bars"></span> Menu
+                </button>
+                <div class="collapse navbar-collapse" id="ftco-nav">
+                    <ul class="navbar-nav m-auto">
+                        <li class="nav-item active"><a href="{{url('/')}}" class="nav-link">Home</a></li>
+                        <li class="nav-item"><a href="about-us" class="nav-link">About Us</a></li>
+                        <li class="nav-item"><a href="our-aim" class="nav-link">Our Aim</a></li>
+                        <li class="nav-item"><a href="our-services" class="nav-link">Services</a></li>
+                        <li class="nav-item"><a href="portfolio" class="nav-link">Portfolio</a></li>
+                        <!-- <li class="nav-item"><a href="cases.php" class="nav-link">Case Study</a></li> -->
+                        <li class="nav-item"><a href="blog.php" class="nav-link">Blogs</a></li>
+                        <li class="nav-item"><a href="testimonials" class="nav-link">Testimonials</a></li>
+                        <li class="nav-item"><a href="contact-us" class="nav-link">Contact</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </div>
+    <div id="header-2" class="fixed-top">
+        <div class="wrap" style="z-index: 100; position:absolute; background: #FFFFFF;">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="bg-wrap">
+                            <div class="row">
+                                <div class="col-md-6 d-flex align-items-center">
+                                    <p class="mb-0 phone pl-md-2">
+                                        <a href="#" class="mr-2" style="color: black;"><span class="fa fa-phone mr-1" style="color: black;"></span> 0020-3813-0607</a>
+                                        <a href="#" style="color: black;"><span class="fa fa-paper-plane mr-1" style="color: black;"></span> info@vedevelopers.com</a>
+                                    </p>
+                                </div>
+                                <div class="col-md-6 d-flex justify-content-md-end">
+                                    <div class="social-media">
+                                        <p class="mb-0 d-flex">
+                                            <a href="#" class="d-flex align-items-center justify-content-center"><span class="fa fa-facebook" style="color: black;"><i class="sr-only">Facebook</i></span></a>
+                                            <a href="#" class="d-flex align-items-center justify-content-center"><span class="fa fa-twitter" style="color: black;"><i class="sr-only">Twitter</i></span></a>
+                                            <a href="#" class="d-flex align-items-center justify-content-center"><span class="fab fa-linkedin" style="color: black;"></span><i class="sr-only">Linkedin</i></span></a>
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <nav class="navbar navbar-expand-lg navbar-dark ftco_navbar bg-dark ftco-navbar-light" id="ftco-navbar" style="position: absolute; top: 40px; z-index: 100; width: 100%; background: #FFFFFF !important;">
             <div class="container">
                 <a class="navbar-brand" href="{{url('/')}}"><img class="img-fluid" src="public/assets/ved-logo-2.png"></a>
                 <!-- <form action="#" class="searchform order-sm-start order-lg-last">
